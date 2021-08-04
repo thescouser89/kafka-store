62d3823f9be2df3da9c919264e892a1d92030058
Dustin Kut Moy Cheung <dcheung@redhat.com>
2021-08-04 16:27:35 -0400

    Update to Quarkus 2.1.1

--- src/test/java/org/jboss/pnc/kafkastore/kafka/KafkaMessageGenerator.java
+++ src/test/java/org/jboss/pnc/kafkastore/kafka/KafkaMessageGenerator.java
@@ -17,7 +17,7 @@
  */
 package org.jboss.pnc.kafkastore.kafka;
 
-import io.reactivex.Flowable;
+import io.smallrye.mutiny.Multi;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.pnc.kafkastore.dto.ingest.KafkaMessageDTO;
@@ -27,7 +27,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 @ApplicationScoped
 @Slf4j
@@ -39,19 +39,20 @@ public class KafkaMessageGenerator {
     BuildStageRecordMapper mapper;
 
     @Outgoing("duration")
-    public Flowable<String> generate() {
+    public Multi<String> generate() {
 
-        return Flowable.interval(200, TimeUnit.MILLISECONDS).map(tick -> {
-
+        return Multi.createFrom().ticks().every(Duration.ofMillis(200)).map(tick -> {
             KafkaMessageDTO kafkaMessageDTO = factory.manufacturePojo(KafkaMessageDTO.class);
             kafkaMessageDTO.setLoggerName("org.jboss.pnc._userlog_.process-stage-update");
             kafkaMessageDTO.getMdc().setProcessStageStep("END");
-
-            String data = mapper.toJsonString(kafkaMessageDTO);
-
-            log.debug("Sending: {}", data);
-            return data;
+            try {
+                String data = mapper.toJsonString(kafkaMessageDTO);
+                log.debug("Sending: {}", data);
+                return data;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "";
+            }
         });
     }
-
 }
