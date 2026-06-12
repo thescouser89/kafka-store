de7f1d785a87852b436a3636e813e8704e025eb5
Dustin Kut Moy Cheung <dcheung@redhat.com>
2026-06-12 14:19:24 -0400

    Add startup script for container image

---
+++ src/main/jib/home/jboss/run-java.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# JBoss, Home of Professional Open Source.
+# Copyright 2019 Red Hat, Inc., and individual contributors
+# as indicated by the @author tags.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+# TODO: once we move to external-secrets operator, we can in theory remove
+#       that script
+
+# NCL-7125: avoid using openshift secrets with app secrets
+export SECRETS_PATH="/mnt/secrets"
+export QUARKUS_DATASOURCE_JDBC_URL="$(cat ${SECRETS_PATH}/${KAFKA_STORE_SECRET_NAME}-${APP_ENV}/datasource.connection)"
+export QUARKUS_DATASOURCE_USERNAME="$(cat ${SECRETS_PATH}/${KAFKA_STORE_SECRET_NAME}-${APP_ENV}/datasource.user)"
+export QUARKUS_DATASOURCE_PASSWORD="$(cat ${SECRETS_PATH}/${KAFKA_STORE_SECRET_NAME}-${APP_ENV}/datasource.password)"
+
+echo "Datasource url: ${QUARKUS_DATASOURCE_JDBC_URL}"
+
+export KAFKA_JAAS_CONFIG="$(cat ${SECRETS_PATH}/${KAFKA_CLIENT_SECRET_NAME}-${APP_ENV}/kafka_jaas_conf)"
+
+java \
+    -Djava.util.logging.manager=org.jboss.logmanager.LogManager
+    -jar quarkus-run.jar
